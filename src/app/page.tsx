bf46280ee91ba5b42d95b243c4989fe52ae3da63
ldb <ldb0316@ictway.co.kr>
2026-03-04 05:43:59 +0000

    Feat: prettier 설정 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello World1</h1>
+      <h1>Hello World2</h1>
+      <h1>Hello World3</h1>
+      <h1>Hello World4</h1>
+      <h1>Hello World5</h1>
+      <h1>Hello World6</h1>
     </div>
-  );
+  )
 }

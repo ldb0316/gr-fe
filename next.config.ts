8456939eb55526c131924ebffd7d5e05d9f659fe
ldb <ldb0316@ictway.co.kr>
2026-03-05 05:53:39 +0000

    Refactor: 로그성 코드 제거

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,10 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
 
   async rewrites() {
-    const backendUrl = process.env.API_DOMAIN || 'http://host.docker.internal:8080';
-    console.log(`📡 Rewriting /api-be to: ${backendUrl}`);
     return [
       {
         source: "/api-be/:path*",
-        destination: `${backendUrl}/:path*`,
+        destination: `${process.env.API_DOMAIN}/:path*`,
       },
     ];
   }

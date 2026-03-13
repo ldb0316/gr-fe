5a04737a5c1e93e428c6ffd4b2f85580ba6ddbaa
ldb <ldb0316@ictway.co.kr>
2026-03-13 01:41:55 +0000

    Feat: 샘플페이지 구분좀더쉽게변경

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -8,7 +8,6 @@ import { useState } from 'react'
 
 const SamplePage = () => {
   const [apiData, setData] = useState('')
-  const { menus } = useMenuStore((state) => state)
   const router = useRouter()
   const requestApi = async () => {
     const data = await customFetch('/api-be/adm/menu', {
@@ -22,22 +21,25 @@ const SamplePage = () => {
   const routeToMain = () => {
     router.push('/main')
   }
+
+  const ldbArt = `
+  _        ____    ____  
+ | |      |  _ \\  | __ ) 
+ | |      | | | | |  _ \\ 
+ | |___   | |_| | | |_) |
+ |_____|  |____/  |____/ 
+`
+
   return (
     <>
-      <h1>하이</h1>
+      <h1>여기는 샘플페이지임..</h1>
       <Button onClick={requestApi}>api요청</Button>
       <Button onClick={routeToSignin}>로그인페이지</Button>
       <Button onClick={routeToMain}>메인페이지</Button>
       <hr />
       <div>{JSON.stringify(apiData)}</div>
       <hr />
-      <div>
-        {menus.map((menu) => (
-          <div key={menu.menuTsid}>
-            {menu.menuNm + '(' + menu.menuAcsAuthrtCd + ')'}
-          </div>
-        ))}
-      </div>
+      <pre>{ldbArt}</pre>
     </>
   )
 }

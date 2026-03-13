2f852171b992d83e1a35ec2ccc6136f5ec9fa5b0
ldb <ldb0316@ictway.co.kr>
2026-03-13 01:38:05 +0000

    Feat: 기본경로 main으로 리다이렉트

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,25 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname
+  const { nextUrl, cookies, url } = request
+
   try {
-    const hasRefreshToken =
-      request.cookies.get('hasRefreshToken')?.value === 'true'
+    if (nextUrl.pathname === '/')
+      // 기본경로로 들어오면 main으로 리다이렉트
+      return NextResponse.redirect(new URL('/main', url))
+
+    const hasRefreshToken = cookies.get('hasRefreshToken')?.value === 'true'
     const blackListWhenSignedIn = ['/user/signin', '/user/signup']
 
-    if (hasRefreshToken && blackListWhenSignedIn.includes(pathname)) {
-      return NextResponse.redirect(new URL('/main', request.url))
+    if (hasRefreshToken && blackListWhenSignedIn.includes(nextUrl.pathname)) {
+      return NextResponse.redirect(new URL('/main', url))
     }
 
     return NextResponse.next()
   } catch (error) {
     console.error('middleware error', error)
-    if (pathname === '/error') return NextResponse.next() // 무한루프방지
-    const errorUrl = new URL('/error', request.url)
+    if (nextUrl.pathname === '/error') return NextResponse.next() // 무한루프방지
+    const errorUrl = new URL('/error', url)
     return NextResponse.rewrite(errorUrl) // 기존 URL 그대로 노출
   }
 }
